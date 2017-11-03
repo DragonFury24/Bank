7d8f3092337780e6bb403dd164635320ec386d00
DragonFury24 <dennisdnguyen19@gmail.com>
2017-11-03 09:24:09 -0400

    .

--- src/Bank.java
+++ src/Bank.java
@@ -8,13 +8,16 @@ public class Bank {
         String input;
         boolean exited = false;
 
+        //Ask for name
         System.out.println("What is your name?");
         name = keyboard.nextLine();
+        //Ask for initial balance
         System.out.println("What is your initial balance?");
         balance = keyboard.nextDouble();
         keyboard.nextLine();
-        BankAccount bankAccount = new BankAccount(name, balance);
+        BankAccount bankAccount = new BankAccount(name, balance); //Create new BankAccount object
         while (!exited) {
+            //Display optiona
             System.out.println();
             System.out.println("Please type in the number of the option you want to choose");
             System.out.println("1. Retrieve account information (name and balance).");
@@ -27,17 +30,17 @@ public class Bank {
                 case "1":
                     System.out.println(bankAccount.toString());
                     break;
-                case "2":
+                case "2": //Deposit
                     System.out.println("Please input the amount you would like to deposit.");
                     bankAccount.deposit(keyboard.nextDouble());
                     keyboard.nextLine();
                     break;
-                case "3":
+                case "3": //Withdrawal
                     System.out.println("Please input the amount you would like to withdraw.");
                     bankAccount.withdraw(keyboard.nextDouble());
                     keyboard.nextLine();
                     break;
-                case "4":
+                case "4": //Exit bank
                     exited = true;
                     break;
                 default:
--- src/BankAccount.java
+++ src/BankAccount.java
@@ -7,30 +7,33 @@ public class BankAccount {
     private String ownerName;
     private double balance;
 
+    //return owner name
     public String getOwnerName() {
         return ownerName;
     }
 
+    //return balance
     public double getBalance() {
         return balance;
     }
 
     public double deposit(double deposit) {
-        balance +=deposit;
+        balance +=deposit; //Add deposit amount to balance
         return deposit;
     }
 
     public double withdraw(double withdraw) { //Withdraws specified amount from @balance and returns withdrawal amount. Returns -1 if @balance is less than withdrawal
+
         if (balance - withdraw < 0) {
             System.out.println("You do not have enough funds to withdraw this much.");
             System.out.println("Your balance has remained unchanged.");
             return -1;
         }
-        balance -= withdraw;
-        return withdraw;
+        balance -= withdraw; //subtract withdrawal amount from the balance
+        return withdraw; //returns the amount withdrawed
     }
 
     public String toString() { //Returns @ownerName and @balance of BankAccount
-        return "[" + ownerName.trim() + ", " + "$" + balance + "]";
+        return "[" + ownerName.trim() + ", " + "$" + balance + "]"; //return name and balance
     }
 }
